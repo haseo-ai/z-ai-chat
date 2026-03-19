65e4c0bfd5c0a2374a93d36cb0c74ebe6fad5774
Z User <z@container>
2026-03-19 11:57:28 +0000

    style: 패딩 최소화 (1px)

--- android/app/src/main/java/com/zai/chat/MainActivity.java
+++ android/app/src/main/java/com/zai/chat/MainActivity.java
@@ -3,7 +3,6 @@ package com.zai.chat;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowInsets;
-import android.widget.FrameLayout;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.graphics.Insets;
@@ -13,8 +12,6 @@ import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     
-    private View contentView;
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // Edge-to-Edge 활성화
@@ -22,25 +19,17 @@ public class MainActivity extends BridgeActivity {
         
         super.onCreate(savedInstanceState);
         
-        // 시스템 바 높이만큼 패딩 적용
-        applySystemBarPadding();
+        // 최소 패딩 적용 (1px)
+        applyMinimalPadding();
     }
     
-    private void applySystemBarPadding() {
-        contentView = findViewById(android.R.id.content);
+    private void applyMinimalPadding() {
+        View contentView = findViewById(android.R.id.content);
         if (contentView == null) return;
         
         ViewCompat.setOnApplyWindowInsetsListener(contentView, (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            
-            // 시스템 바 높이의 2배만큼 패딩 적용
-            int topPadding = systemBars.top * 2;
-            int bottomPadding = systemBars.bottom * 2;
-            int leftPadding = systemBars.left * 2;
-            int rightPadding = systemBars.right * 2;
-            
-            v.setPadding(leftPadding, topPadding, rightPadding, bottomPadding);
-            
+            // 1px 패딩
+            v.setPadding(1, 1, 1, 1);
             return insets;
         });
     }
